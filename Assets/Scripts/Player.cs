0daecb1901ac219b0108fee310260914450626a7
AnssiRem <31952830+AnssiRem@users.noreply.github.com>
2018-03-23 19:10:32 +0200

    add first test

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float MovementSpeed;
+    public float RotationSpeed;
+
+    public float MaxProximity;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        var mov = Input.GetAxis("Vertical") * Time.deltaTime * MovementSpeed;
+        var rot = Input.GetAxis("Horizontal") * Time.deltaTime * RotationSpeed;
+
+        transform.Translate(0, mov, 0);
+        transform.Rotate(0, 0, rot);
+    }
+}
